3eae75bdf3ad19689ce23ab26ad0bc9ff578b46d
Sreelakshmi <72209376+Sreeelakshmi@users.noreply.github.com>
2025-01-13 19:11:34 +0530

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,33 +1,8 @@
-import streamlit as st
+iimport streamlit as st
 import openai
 import pandas as pd
 from llama_index.llms.openai import OpenAI
 from llama_index import VectorStoreIndex, Document, Settings
-
-# Streamlit page configuration
-st.set_page_config(
-    page_title="Chat with CSV Data, powered by LlamaIndex",
-    page_icon="🦙",
-    layout="centered",
-    initial_sidebar_state="auto",
-    menu_items=None,
-)
-
-openai.api_key = st.secrets.openai_key
-st.title("Chat with Your CSV Data, powered by LlamaIndex 💬🦙")
-st.info(
-    "Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)",
-    icon="📃",
-)
-
-# Initialize chat messages history
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {
-            "role": "assistant",
-            "content": "Ask me a question about the uploaded CSV data!",
-        }
-    ]
 
 @st.cache_resource(show_spinner=False)
 def load_csv_data(file_path):
@@ -49,31 +24,39 @@ def load_csv_data(file_path):
     index = VectorStoreIndex.from_documents(documents)
     return index
 
+def start_chatbot():
+    st.subheader("Chat with Your CSV Data, powered by LlamaIndex 💬🦙")
+    uploaded_file = st.file_uploader("data", type=["csv"])
 
-# File uploader in Streamlit
-uploaded_file = st.file_uploader("data", type=["csv"])
+    if uploaded_file:
+        index = load_csv_data(uploaded_file)
+        if "chat_engine" not in st.session_state.keys():
+            st.session_state.chat_engine = index.as_chat_engine(
+                chat_mode="condense_question", verbose=True, streaming=True
+            )
 
-if uploaded_file:
-    index = load_csv_data(uploaded_file)
-    if "chat_engine" not in st.session_state.keys():
-        st.session_state.chat_engine = index.as_chat_engine(
-            chat_mode="condense_question", verbose=True, streaming=True
-        )
+        if "messages" not in st.session_state.keys():
+            st.session_state.messages = [
+                {
+                    "role": "assistant",
+                    "content": "Ask me a question about the uploaded CSV data!",
+                }
+            ]
 
-    if prompt := st.chat_input("Ask a question about the CSV data"):  # User input
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        if prompt := st.chat_input("Ask a question about the CSV data"):  # User input
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Display message history
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.write(message["content"])
+        # Display message history
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
 
-    # Generate assistant's response
-    if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            response_stream = st.session_state.chat_engine.stream_chat(prompt)
-            st.write_stream(response_stream.response_gen)
-            message = {"role": "assistant", "content": response_stream.response}
-            st.session_state.messages.append(message)
-else:
-    st.warning("Please upload a CSV file to get started.")
+        # Generate assistant's response
+        if st.session_state.messages[-1]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                response_stream = st.session_state.chat_engine.stream_chat(prompt)
+                st.write_stream(response_stream.response_gen)
+                message = {"role": "assistant", "content": response_stream.response}
+                st.session_state.messages.append(message)
+    else:
+        st.warning("Please upload a CSV file to get started.")
